bb2e10fbc674dd9bc0e074b780eff0b9fb03fa06
Sergio Cambelo <sergio@cambelo.com>
2023-12-16 23:04:36 +0100

    Initial commit

---
+++ test/aws-cloudfront-spa-routing.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCloudfrontSpaRouting from '../lib/aws-cloudfront-spa-routing-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cloudfront-spa-routing-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCloudfrontSpaRouting.AwsCloudfrontSpaRoutingStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
